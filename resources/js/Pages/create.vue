9d3664ec5e70d7bf600a1bdbfbbae336687d507f
Eduardo Lazzaretti <duadsal@gmail.com>
2024-01-24 19:38:09 -0300

    Initializing de view create

---
+++ resources/js/Pages/create.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card col-6 offset-3">
+        <div class="card-header">
+            Featured
+        </div>
+        <div class="card-body">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                    <input type="name" id="name" v-model="form.name" class="form-control" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Email</label>
+                    <input type="email" id="email" v-model="form.email" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Email</label>
+                    <input type="email" id="email" v-model="form.email" class="form-control">
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
